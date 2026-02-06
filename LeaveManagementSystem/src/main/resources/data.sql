677c1d020900ec69ae1e9e692d1feffe54eef289
sowmi <sowmi@Sowmiya>
2026-02-06 14:41:22 +0530

    commit

---
+++ LeaveManagementSystem/src/main/resources/data.sql
@@ -0,0 +1,11 @@
+-- HR
+INSERT INTO employee (name, project_name, role, password)
+VALUES ('HR Admin', 'Leave Management System', 'HR', 'hr123');
+
+-- Manager
+INSERT INTO employee (name, project_name, role, password)
+VALUES ('Manager One', 'Leave Management System', 'MANAGER', 'manager123');
+
+-- Employee
+INSERT INTO employee (name, project_name, role, password)
+VALUES ('Employee One', 'Leave Management System', 'EMPLOYEE', 'emp123');
